140ff7cd20dd7c7780eab4fa2504396ab449f5fc
Dmitry Mikhaltchenkov <mikhaltchenkov@gmail.com>
2018-10-27 08:28:08 +0300

    Добавьте файлы проекта.

---
+++ Beauty/Beauty/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Beauty.Models;
+
+namespace Beauty.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
